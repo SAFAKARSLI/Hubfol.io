bd19c39401cbeb025c2acb35b2fb9739e7097f8d
SAFAKARSLI <safakarsli.com@gmail.com>
2024-08-15 20:21:19 -0400

    Initial nextauth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Projects from '@/components/Projects'
+import { getServerSession } from "next-auth"
 
-export default function page() {
-  return <div>NO_ACTIVE_PROJECT (HOME PAGE)</div>
+export default async function page() {
+  const session = await getServerSession()
+
+  return (
+    <div>
+      <div>Name: {session?.user?.name}</div>
+      <div>Email: {session?.user?.email}</div>
+      <div>Image: {session?.user?.image}</div>
+    </div>
+  )
 }
