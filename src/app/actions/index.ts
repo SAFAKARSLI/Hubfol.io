6cce7d3e35ff7a42508a7f930444b53a900081f0
SAFAKARSLI <safakarsli.com@gmail.com>
2024-08-15 20:22:31 -0400

    Setup for new database architecture

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -6,13 +6,13 @@ import { ObjectId } from 'mongodb';
 export const getProjects = async () => {
   await client.connect()
   // const projects = await client.db('projects').collection('dev').find({userId: 123}, {projection: {title: 1, tagline: 1, iconLink: 1}}).toArray();
-  const projects = await client.db('projects').collection('dev').find({userId: 123}).toArray();
+  const projects = await client.db('dev').collection('projects').find({userId: 123}).toArray();
 
   return JSON.parse(JSON.stringify(projects));
 }
 
 export const getProject = async (_id: string) => {
   await client.connect();
-  const project = await client.db('projects').collection('dev').findOne({_id: new ObjectId(_id)});
+  const project = await client.db('dev').collection('projects').findOne({_id: new ObjectId(_id)});
   return JSON.parse(JSON.stringify(project));
 }
