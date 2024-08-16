97ad0dbb5f6c16d2c75d40e3d08c730ce098e24f
SAFAKARSLI <safakarsli.com@gmail.com>
2024-08-16 02:55:10 -0400

    Add basic signin-signout functionality

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProfileOverview from './ProfileOverview'
-import { Flex, Text, Button } from '@radix-ui/themes'
+import { Flex, Text, Button, Link as RadixLink, DropdownMenu } from '@radix-ui/themes'
 import Link from 'next/link'
-import { getServerSession } from 'next-auth'
-import { signOut, signIn } from 'next-auth/react'
+import { FaGoogle } from "react-icons/fa";
+import { EnvelopeClosedIcon } from '@radix-ui/react-icons'
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 type Props = {}
 
@@ -14,7 +16,10 @@ const links: string[] = [
   'Certificates and Education'
 ]
 
-const TopBar: React.FC<Props> = ({}) => {
+const TopBar: React.FC<Props> = async ({}) => {
+
+  const session = await getServerSession(authOptions)
+
   return (
     <Flex className=''>
         <ProfileOverview 
@@ -23,13 +28,35 @@ const TopBar: React.FC<Props> = ({}) => {
             location='San Francisco, CA, USA' 
             contactInfo='johndoe@hubfol.io'
         />
-        <Flex justify={"center"} width={"100%"} className='border-y border-gray-4 w-full bg-gray-1' >
-          <Flex justify={"between"} align={"center"} height={"100%"} width={"60%"}>
+        <Flex px={"8rem"} justify={"between"} className='border-y border-gray-4 w-full bg-gray-1' >
+          <Flex justify={"between"} align={"center"} height={"100%"} width={"50%"}>
           {links.map((link, i) => (
             <Text as={'div'} size={"2"} color='gray' key={i} className='hover:text-white'>
               <Link href={"/"+link.toLowerCase().replaceAll(' ', '-')}>{link}</Link>
             </Text>
           ))}
+          </Flex>
+          <Flex gap={"8rem"} align={"center"}>
+            <Button size={"3"}>
+              <EnvelopeClosedIcon /> Send Proposal</Button>
+
+            {
+              !session?.user ? <DropdownMenu.Root>
+                <DropdownMenu.Trigger >
+                  <Button variant="ghost" size={"3"}>
+                    Sign In
+                    <DropdownMenu.TriggerIcon />
+                  </Button>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Item><FaGoogle /> <Link href={"api/auth/signin"}>Sign In With Google</Link></DropdownMenu.Item>
+
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+              :
+              <RadixLink href={"api/auth/signout"}>Sign Out</RadixLink>
+            }
+                          
           </Flex>
         </Flex>
     </Flex>
