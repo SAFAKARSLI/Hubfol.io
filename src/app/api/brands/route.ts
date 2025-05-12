0a569f188966fc037f3c905b71dda5c9127272b2
SAFAKARSLI <safakarsli.com@gmail.com>
2025-05-12 13:03:28 -0400

    api/brands with extra conditional logic

--- src/app/api/brands/route.ts
+++ src/app/api/brands/route.ts
@@ -1,22 +1,24 @@
-import { prisma } from '@/db';
-import { Brand } from '@/types/brand';
+import { prisma } from "@/db";
+import { Brand } from "@/types/brand";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const query = searchParams.get('query');
-
-  if (!query) {
-    return new Response('No query provided', { status: 400 });
-  }
-
   try {
+    const url = request?.url ? new URL(request.url) : null;
+    const query = url?.searchParams.get("query");
+
+    if (!query) {
+      return new Response("No query provided", { status: 400 });
+    }
+
     const brands = (await prisma.brandIcons.findMany({
       take: 30,
-      where: { brand_name: { contains: query, mode: 'insensitive' } },
+      where: { brand_name: { contains: query, mode: "insensitive" } },
     })) as Brand[];
+
     return new Response(JSON.stringify(brands), { status: 200 });
   } catch (error) {
-    return new Response('Internal Server Error. Failed to fetch sections', {
+    console.error("API Error:", error);
+    return new Response("Internal Server Error. Failed to fetch sections", {
       status: 500,
     });
   } finally {
