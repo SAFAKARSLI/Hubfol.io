d58d9e618e1be4f3937c8b7e7a69fbe82a7bb849
SAFAKARSLI <safakarsli.com@gmail.com>
2025-05-13 18:20:13 -0400

    section not founding fixed

--- src/app/api/sections/route.ts
+++ src/app/api/sections/route.ts
@@ -1,21 +1,29 @@
 import { validateUUID } from "@/utils";
 import { SectionRepository } from "@/db";
+import Section from "@/types/section";
 
 const sectionRepository = new SectionRepository();
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const projectSlug = searchParams.get("projectSlug");
+  const projectUUID = searchParams.get("projectUUID");
 
   try {
-    const sections = await sectionRepository.findSectionsByProjectSlug(
-      projectSlug as string
-    );
+    var sections: Section[] = [];
+    if (projectSlug) {
+      sections = await sectionRepository.findSectionsByProjectSlug(
+        projectSlug as string
+      );
+    } else if (projectUUID) {
+      sections = await sectionRepository.findSectionsByProjectUuid(
+        projectUUID as string
+      );
+    }
 
     if (!sections) {
       return new Response("No sections found", { status: 404 });
     }
-
     return new Response(JSON.stringify(sections), { status: 200 });
   } catch (error) {
     return new Response("Internal Server Error. Failed to fetch sections", {
